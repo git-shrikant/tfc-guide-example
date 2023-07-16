2244c29bfb65c6bf44733e60349093db386e6fee
git-shrikant <53266600+git-shrikant@users.noreply.github.com>
2023-07-16 22:41:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ organization_name = "example-org-e4df2b"
 }
 
 resource "tfe_variable_set" "test_spc" {
-  name         = "Admin"
+  name         = "test_spc"
   description  = "Common variables for admin workspaces"
   organization = local.organization_name
 }
