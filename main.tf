9ab47ab419b653c4061e11be65ea99bab0716b28
git-shrikant <53266600+git-shrikant@users.noreply.github.com>
2023-07-17 18:27:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ data "tfe_organization" "test" {
   name  = local.organisation_name
 }
 
-resource "tfe_workspace" "test" {
+data "tfe_workspace" "test" {
   name         = "test_spc"
   organization = data.tfe_organization.test.id
 }
@@ -14,7 +14,7 @@ resource "tfe_variable" "test" {
   key          = "TF_VAR_1"
   value        = "Shrikant"
   category     = "terraform"
-  workspace_id = tfe_workspace.test.id
+  workspace_id = data.tfe_workspace.test.id
   description  = "a useful description"
 }
  
