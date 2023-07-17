7ca8d0fe9f6479f127a2483304cbfb1f6118c647
git-shrikant <53266600+git-shrikant@users.noreply.github.com>
2023-07-17 18:22:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 locals{
 organisation_name = "example-org-e4df2b"
 }
-resource "tfe_organization" "test" {
+data "tfe_organization" "test" {
   name  = local.organisation_name
   email = "shrikant.khupat1@gmail.com"
 }
 
 resource "tfe_workspace" "test" {
   name         = "test_spc"
-  organization = tfe_organization.test.id
+  organization = data.tfe_organization.test.id
 }
 
 resource "tfe_variable" "test" {
