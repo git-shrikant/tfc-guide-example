a9c786962793e400824b7862df5777ad0536675c
git-shrikant <53266600+git-shrikant@users.noreply.github.com>
2023-07-17 17:19:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,24 @@
 locals{
-organization_name = "git-shrikant"
+organization_name = "example-org-e4df2b"
 }
 
-resource "tfe_variable_set" "test_spc" {
-  name         = "test_spc"
-  description  = "Common variables for admin workspaces"
-  organization = local.organization_name
+resource "tfe_variable" "admin_variable_set_variables" {
+  for_each = {
+    # github provider
+    "GITHUB_APP_ID" : "111",
+    "GITHUB_APP_INSTALLATION_ID" : "111"
+    "GITHUB_OWNER" : "git-shrikant"
+    # google / google-beta provider
+    
+    # tfe provider
+   # "TFE_ORGANIZATION" : local.organisation_name
+    # other
+    "TF_VAR_tfc_organisation_name" : local.organization_name,
+  }
+
+  key             = each.key
+  value           = each.value
+  category        = "env"
+  variable_set_id = "ws-abZuN4Tr9xopwEp9"
 }
 
