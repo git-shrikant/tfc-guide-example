d207f5016e12cf14320407619c1956b60fdca575
git-shrikant <53266600+git-shrikant@users.noreply.github.com>
2023-07-17 17:12:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,8 +7,4 @@ resource "tfe_variable_set" "test_spc" {
   description  = "Common variables for admin workspaces"
   organization = local.organization_name
 }
-  key  = each.key
-  value           = each.value
-  category        = "env"
- # variable_set_id = tfe_variable_set.test_spc.id
-}
+
