5c49dc14c3878b980ae65e3e8d958e68a0646709
git-shrikant <53266600+git-shrikant@users.noreply.github.com>
2023-07-17 18:20:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,27 +1,22 @@
 locals{
 organisation_name = "example-org-e4df2b"
 }
-resource "tfe_variable_set" "test" {
-  name         = "admin"
-  organization = local.organisation_name
+resource "tfe_organization" "test" {
+  name  = local.organisation_name
+  email = "shrikant.khupat1@gmail.com"
 }
-resource "tfe_variable" "admin_variable_set_variables" {
-  for_each = {
-    # github provider
-    "GITHUB_APP_ID" : "111",
-    "GITHUB_APP_INSTALLATION_ID" : "111"
-    "GITHUB_OWNER" : "git-shrikant"
-    # google / google-beta provider
-    
-    # tfe provider
-   # "TFE_ORGANIZATION" : local.organisation_name
-    # other
-    "TF_VAR_tfc_organisation_name" : local.organisation_name,
-  }
 
-  key             = each.key
-  value           = each.value
-  category        = "env"
-  variable_set_id = tfe_variable_set.test.id 
+resource "tfe_workspace" "test" {
+  name         = "test_spc"
+  organization = tfe_organization.test.id
 }
+
+resource "tfe_variable" "test" {
+  key          = "TF_VAR_1"
+  value        = "Shrikant"
+  category     = "terraform"
+  workspace_id = tfe_workspace.test.id
+  description  = "a useful description"
+}
+ 
 
